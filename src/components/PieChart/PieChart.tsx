4896260b5d5953cabf287cf77f75a403bb477e1a
Olga Polikashina <olga.polikashina@gmail.com>
2020-08-15 13:37:53 +0300

    download wheel in pdf

--- src/components/PieChart/PieChart.tsx
+++ src/components/PieChart/PieChart.tsx
@@ -1,42 +1,42 @@
 import React, { useCallback } from "react";
 import partialCircle from "svg-partial-circle";
-import './PieChart.scss'
+import "./PieChart.scss";
 
 export type ChartItem = {
-  id: number
-  title: string
-  value: number
-  color: string
-}
+  id: number;
+  title: string;
+  value: number;
+  color: string;
+};
 
 type Props = {
-  items: ChartItem[]
-  viewBoxSize: number
-  onClick: (id: number) => void
+  items: ChartItem[];
+  viewBoxSize: number;
+  onClick: (id: number) => void;
 };
 
 type Сoordinate = {
-  x: number
-  y: number
+  x: number;
+  y: number;
 };
 
 enum ChartTextAlign {
-  Start = 'start',
-  End = 'end',
-  Middle = 'middle'
+  Start = "start",
+  End = "end",
+  Middle = "middle",
 }
 
 interface ChartText extends Сoordinate {
-  align: ChartTextAlign
+  align: ChartTextAlign;
 }
 
 type Path = {
-  id: number
-  path: string
-  title: string
-  color: string
-  text: ChartText
-}
+  id: number;
+  path: string;
+  title: string;
+  color: string;
+  text: ChartText;
+};
 
 const getDegrees = (radius: number): number => {
   return (Math.PI / 180) * radius;
@@ -46,9 +46,9 @@ const PieChart: React.FC<Props> = props => {
   const { items, viewBoxSize, onClick } = props;
   const degreeStart = -90;
   const chartPath: Path[] = [];
-  const canvasSize = 600
-  const chartSize = viewBoxSize
-  const TEXT_GAP = 20
+  const canvasSize = 600;
+  const chartSize = viewBoxSize;
+  const TEXT_GAP = 20;
 
   const getRadius = useCallback(
     (value: number): number => {
@@ -62,12 +62,25 @@ const PieChart: React.FC<Props> = props => {
 
     if (items.length === 1) {
       const { id, value, title, color } = items[0];
-      const radius = getRadius(value)
+      const radius = getRadius(value);
 
       return (
         <>
-          <circle cx={center.x} cy={center.y} r={radius} fill={color} className="pie-chart__sector" onClick={() => onClick(id)}/>
-          <text x={center.x} y={center.y - radius - TEXT_GAP} textAnchor={ChartTextAlign.Middle} className='pie-chart__text pie-chart__text--middle' onClick={() => onClick(id)}>
+          <circle
+            cx={center.x}
+            cy={center.y}
+            r={radius}
+            fill={color}
+            className="pie-chart__sector"
+            onClick={() => onClick(id)}
+          />
+          <text
+            x={center.x}
+            y={center.y - radius - TEXT_GAP}
+            textAnchor={ChartTextAlign.Middle}
+            className="pie-chart__text pie-chart__text--middle"
+            onClick={() => onClick(id)}
+          >
             {title} <tspan>{value}</tspan>
           </text>
         </>
@@ -81,28 +94,28 @@ const PieChart: React.FC<Props> = props => {
     items.forEach(({ id, title, value, color }, index) => {
       angleEnd = getDegrees(degreeStart + itemSize * (index + 1));
       const path = partialCircle(center.x, center.y, getRadius(value), angleStart, angleEnd);
-      const sectorEndY = Number(path[1][7])
-      const sectorStartY = Number(path[0][2])
-      let text: ChartText
+      const sectorEndY = Number(path[1][7]);
+      const sectorStartY = Number(path[0][2]);
+      let text: ChartText;
 
-      if  (sectorEndY === sectorStartY) {
+      if (sectorEndY === sectorStartY) {
         text = {
           x: center.x,
           y: center.y + getRadius(value) + TEXT_GAP * 2,
-          align: ChartTextAlign.Middle
-        }
-      } else if  (sectorEndY > sectorStartY) {
+          align: ChartTextAlign.Middle,
+        };
+      } else if (sectorEndY > sectorStartY) {
         text = {
           x: center.x + getRadius(value) + TEXT_GAP,
-          y: (sectorEndY - sectorStartY ) / 2 + sectorStartY,
-          align: ChartTextAlign.Start
-        }
+          y: (sectorEndY - sectorStartY) / 2 + sectorStartY,
+          align: ChartTextAlign.Start,
+        };
       } else {
         text = {
           x: center.x - getRadius(value) - TEXT_GAP,
           y: (sectorStartY - sectorEndY) / 2 + sectorEndY,
-          align: ChartTextAlign.End
-        }
+          align: ChartTextAlign.End,
+        };
       }
 
       chartPath.push({
@@ -110,7 +123,7 @@ const PieChart: React.FC<Props> = props => {
         color,
         title,
         path: path.map(p => p.join(" ")).join(" ") + ` L ${center.x} ${center.y} Z`,
-        text
+        text,
       });
       angleStart = angleEnd;
     });
@@ -118,8 +131,22 @@ const PieChart: React.FC<Props> = props => {
     return chartPath.map(({ id, color, path, title, text }, index) => {
       return (
         <>
-          <path key={`area-${index}`} d={path} strokeWidth={2} fill={color} className="pie-chart__sector" onClick={() => onClick(id)}></path>
-          <text key={`text-${index}`} x={text.x} y={text.y} textAnchor={text.align} className={`pie-chart__text pie-chart__text--${text.align}`} onClick={() => onClick(id)}>
+          <path
+            key={`area-${index}`}
+            d={path}
+            strokeWidth={2}
+            fill={color}
+            className="pie-chart__sector"
+            onClick={() => onClick(id)}
+          ></path>
+          <text
+            key={`text-${index}`}
+            x={text.x}
+            y={text.y}
+            textAnchor={text.align}
+            className={`pie-chart__text pie-chart__text--${text.align}`}
+            onClick={() => onClick(id)}
+          >
             {title} <tspan>{items[index].value}</tspan>
           </text>
         </>
@@ -127,9 +154,9 @@ const PieChart: React.FC<Props> = props => {
     });
   }, [items, onClick]);
 
-
   return (
     <svg
+      xmlns="http://www.w3.org/2000/svg"
       viewBox={`0 0 ${canvasSize} ${canvasSize}`}
       width="100%"
       height="100%"
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Form } from "../Form/Form";
 import { Button } from "../Button/Button";
 import { connect } from "react-redux";
@@ -6,11 +7,13 @@ import { State } from "../../Store/Types";
 import { isEditableMode } from "../../Store/Reducers/Mode";
 import { ActionTypes } from "../../Store/Reducers/Area";
 import { ActionTypes as ModeActionTypes } from "../../Store/Reducers/Mode";
+import { downloadPdf } from "../../helpers";
 
 type Props = StateProps & DispatchProps;
 
 const SidebarPresenter: React.FC<Props> = props => {
   const { isEditingMode, changeMode, selectItem, deleteItem } = props;
+  const hasItems = useSelector((state: State) => !!state.area.items.length);
 
   const addNewAreaHandler = () => {
     changeMode(false);
@@ -22,6 +25,11 @@ const SidebarPresenter: React.FC<Props> = props => {
     deleteItem();
   };
 
+  const downloadHandler = async () => {
+    // TODO: replace with ref
+    downloadPdf(document.querySelector("svg"));
+  };
+
   return (
     <div>
       <Form />
@@ -31,6 +39,13 @@ const SidebarPresenter: React.FC<Props> = props => {
           <br />
           <br />
           <Button onClick={addNewAreaHandler}>Add new area</Button>
+        </>
+      )}
+      {hasItems && (
+        <>
+          <br />
+          <br />
+          <Button onClick={downloadHandler}>Download</Button>
         </>
       )}
     </div>
