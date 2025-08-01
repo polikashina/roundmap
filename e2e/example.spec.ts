d174ad492b790577d757647e27077bd2932b7c7d
Olga Polikashina <olga.polikashina@gmail.com>
2025-08-01 22:14:58 +0300

    test: skip test

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,13 +1,13 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+// test("has title", async ({ page }) => {
+//   await page.goto("https://playwright.dev/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+//   // Expect a title "to contain" a substring.
+//   await expect(page).toHaveTitle(/Playwright/);
 
-  await expect(page).toHaveScreenshot();
-});
+//   await expect(page).toHaveScreenshot();
+// });
 
 test("get started link", async ({ page }) => {
   await page.goto("https://playwright.dev/");
