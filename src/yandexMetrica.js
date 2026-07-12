ac1d58fc34d606521c167db6c2616fcc04b871cc
Olga Polikashina <olga.polikashina@gmail.com>
2026-07-12 15:54:14 +0300

    chore: update metrica counter id

--- src/yandexMetrica.js
+++ src/yandexMetrica.js
@@ -1,22 +1,22 @@
 export const yandexMetrica = () => {
-  console.log("init metrica");
-  (function(m, e, t, r, i, k, a) {
-    m[i] =
-      m[i] ||
-      function() {
-        (m[i].a = m[i].a || []).push(arguments);
-      };
-    m[i].l = 1 * new Date();
-    (k = e.createElement(t)),
-      (a = e.getElementsByTagName(t)[0]),
-      (k.async = 1),
-      (k.src = r),
-      a.parentNode.insertBefore(k, a);
-  })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    console.log("init metrica");
+    (function(m, e, t, r, i, k, a) {
+        m[i] =
+            m[i] ||
+            function() {
+                (m[i].a = m[i].a || []).push(arguments);
+            };
+        m[i].l = 1 * new Date();
+        (k = e.createElement(t)),
+            (a = e.getElementsByTagName(t)[0]),
+            (k.async = 1),
+            (k.src = r),
+            a.parentNode.insertBefore(k, a);
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-  ym(62606503, "init", {
-    clickmap: true,
-    trackLinks: true,
-    accurateTrackBounce: true,
-  });
+    ym(101053224, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+    });
 };
